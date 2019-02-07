c964ab7de65e2fea1bed654cc1764a00ef1ca8a0
Diep Quynh <remilia.1505@gmail.com>
2019-02-08 00:23:09 +0700

    fbdev: Mark decon IRQs and kthreads as perf critical
    They are responsible for frame rendering. Mark them as critical so
    as to reduce jitter
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    
    Conflicts:
    	drivers/video/fbdev/exynos/dpu/decon_dsi.c

--- drivers/video/fbdev/exynos/dpu/decon_dsi.c
+++ drivers/video/fbdev/exynos/dpu/decon_dsi.c
@@ -145,8 +145,8 @@ int decon_register_irq(struct decon_device *decon)
 		/* Get IRQ resource and register IRQ handler. */
 		/* 0: FIFO irq */
 		res = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
-		ret = devm_request_irq(dev, res->start, decon_irq_handler, 0,
-				pdev->name, decon);
+		ret = devm_request_irq(dev, res->start, decon_irq_handler,
+				IRQF_PERF_CRITICAL, pdev->name, decon);
 		if (ret) {
 			decon_err("failed to install FIFO irq\n");
 			return ret;
@@ -156,7 +156,7 @@ int decon_register_irq(struct decon_device *decon)
 	/* 1: FRAME START */
 	res = platform_get_resource(pdev, IORESOURCE_IRQ, 1);
 	ret = devm_request_irq(dev, res->start, decon_irq_handler,
-			0, pdev->name, decon);
+			IRQF_PERF_CRITICAL, pdev->name, decon);
 	if (ret) {
 		decon_err("failed to install FRAME START irq\n");
 		return ret;
@@ -165,7 +165,7 @@ int decon_register_irq(struct decon_device *decon)
 	/* 2: FRAME DONE */
 	res = platform_get_resource(pdev, IORESOURCE_IRQ, 2);
 	ret = devm_request_irq(dev, res->start, decon_irq_handler,
-			0, pdev->name, decon);
+			IRQF_PERF_CRITICAL, pdev->name, decon);
 	if (ret) {
 		decon_err("failed to install FRAME DONE irq\n");
 		return ret;
@@ -174,7 +174,7 @@ int decon_register_irq(struct decon_device *decon)
 	/* 3: EXTRA: resource conflict, timeout and error irq */
 	res = platform_get_resource(pdev, IORESOURCE_IRQ, 3);
 	ret = devm_request_irq(dev, res->start, decon_irq_handler,
-			0, pdev->name, decon);
+			IRQF_PERF_CRITICAL, pdev->name, decon);
 	if (ret) {
 		decon_err("failed to install EXTRA irq\n");
 		return ret;
@@ -412,7 +412,8 @@ int decon_register_ext_irq(struct decon_device *decon)
 
 	decon_info("%s: gpio(%d)\n", __func__, decon->res.irq);
 	ret = devm_request_irq(dev, decon->res.irq, decon_ext_irq_handler,
-			IRQF_TRIGGER_RISING, pdev->name, decon);
+			IRQF_TRIGGER_RISING | IRQF_PERF_CRITICAL,
+			pdev->name, decon);
 
 	decon->eint_status = 1;
 
@@ -470,7 +471,7 @@ int decon_create_vsync_thread(struct decon_device *decon)
 	}
 
 	sprintf(name, "decon%d-vsync", decon->id);
-	decon->vsync.thread = kthread_run(decon_vsync_thread, decon, name);
+	decon->vsync.thread = kthread_run_perf_critical(decon_vsync_thread, decon, name);
 	if (IS_ERR_OR_NULL(decon->vsync.thread)) {
 		decon_err("failed to run vsync thread\n");
 		decon->vsync.thread = NULL;
