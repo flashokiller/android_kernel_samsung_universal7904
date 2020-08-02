5b4c9cf54cb7a2873493c0b7994c3f2d7722a71e
tytydraco <tylernij@gmail.com>
2020-08-02 04:19:10 -0700

    AnyKernel3: Set ZRAM to half of the accessible system RAM
    Signed-off-by: tytydraco <tylernij@gmail.com>
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- AnyKernel3/patch/services.sh
+++ AnyKernel3/patch/services.sh
@@ -1,7 +1,15 @@
 #!/vendor/bin/sh
-echo 1G > /sys/block/zram0/disksize
-mkswap /dev/block/zram0
-swapon /dev/block/zram0
-echo 80 > /proc/sys/vm/swappiness
-echo 0 > /proc/sys/vm/page-cluster
-echo $(nproc) > /sys/block/zram0/max_comp_streams
+if [[ -d "/sys/block/zram0" ]]
+then
+    memsize=`cat /proc/meminfo | grep "MemTotal" | awk '{print $2}'`
+    halfmemsize=`echo "$memsize/2" | bc`
+
+    swapoff /dev/block/zram0
+    echo 1 > /sys/block/zram0/reset
+    echo ${halfmemsize}KB > /sys/block/zram0/disksize
+    mkswap /dev/block/zram0
+    swapon /dev/block/zram0
+    echo 80 > /proc/sys/vm/swappiness
+    echo 0 > /proc/sys/vm/page-cluster
+    echo $(nproc) > /sys/block/zram0/max_comp_streams
+fi
